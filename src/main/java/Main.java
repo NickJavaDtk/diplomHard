d61888473a11c2a7875cfc93a06eede85dd24ff0
Nickolay Petrushin <nick.mflj@gmail.com>
2022-06-09 22:56:29 +0300

    I took out the logic for the cycle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,11 +13,13 @@ import java.util.List;
 public class Main {
     final static private int port = 8989;
     final static private String path = "pdfs";
+    final static GsonBuilder builder = new GsonBuilder( );
 
     public static void main(String[] args) throws Exception {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File(path));
 
         try (ServerSocket serverSocket = new ServerSocket(port);) {
+            String input;
             while (true) {
                 try (
                         Socket socket = serverSocket.accept( );
@@ -25,22 +27,27 @@ public class Main {
                         PrintWriter printWriter = new PrintWriter(socket.getOutputStream( ));
                 ) {
                     // обработка одного подключения
-                    String input = bufferedReader.readLine( );
+                    input = bufferedReader.readLine( );
                     if (input == null) {
                         break;
                     }
-                    List<PageEntry> list = engine.search(input);
-                    GsonBuilder builder = new GsonBuilder( );
-                    Gson gson = builder.create( );
-                    Type listType = new TypeToken<List<PageEntry>>( ) {
-                    }.getType( );
-                    String json = gson.toJson(list, listType);
-                    printWriter.println(json);
+                    String json = getJson(engine, input);
+                    printWriter.print(json);
+
                 }
             }
         } catch (IOException e) {
             System.out.println("Не могу стартовать сервер");
             e.printStackTrace( );
         }
+    }
+
+    public static String getJson(BooleanSearchEngine engine, String input) {
+        List<PageEntry> list = engine.search(input);
+        Gson gson = builder.create( );
+        Type listType = new TypeToken<List<PageEntry>>( ) {
+        }.getType( );
+        String json = gson.toJson(list, listType);
+        return json;
     }
 }
