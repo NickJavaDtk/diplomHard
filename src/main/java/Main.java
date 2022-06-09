feafbd7613295ad563613d77a425ed3f059bfae5
Nickolay Petrushin <nick.mflj@gmail.com>
2022-06-09 22:30:32 +0300

    corrected

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,28 +12,34 @@ import java.util.List;
 
 public class Main {
     final static private int port = 8989;
+    final static private String path = "pdfs";
 
     public static void main(String[] args) throws Exception {
-        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        try (ServerSocket serverSocket = new ServerSocket(port);
-             Socket socket = serverSocket.accept( );
-             PrintWriter printWriter = new PrintWriter(socket.getOutputStream( ), true);
-             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream( )))) {
-            while (true) {
-                String input = bufferedReader.readLine( );
-                if (input == null) {
-                    break;
-                }
-                List<PageEntry> list = engine.search(input);
-                GsonBuilder builder = new GsonBuilder( );
-                Gson gson = builder.create( );
-                Type listType = new TypeToken<List<PageEntry>>( ) {
-                }.getType( );
-                String json = gson.toJson(list, listType);
-                printWriter.println(json);
-            }
+        BooleanSearchEngine engine = new BooleanSearchEngine(new File(path));
 
+        try (ServerSocket serverSocket = new ServerSocket(port);) {
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept( );
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream( )));
+                        PrintWriter printWriter = new PrintWriter(socket.getOutputStream( ));
+                ) {
+                    // обработка одного подключения
+                    String input = bufferedReader.readLine( );
+                    if (input == null) {
+                        break;
+                    }
+                    List<PageEntry> list = engine.search(input);
+                    GsonBuilder builder = new GsonBuilder( );
+                    Gson gson = builder.create( );
+                    Type listType = new TypeToken<List<PageEntry>>( ) {
+                    }.getType( );
+                    String json = gson.toJson(list, listType);
+                    printWriter.println(json);
+                }
+            }
         } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
             e.printStackTrace( );
         }
     }
